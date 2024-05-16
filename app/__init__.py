6690b39dbd7711718ec6cce08f7f6b9bdc53f0e3
raychristian <raymond.a.christian@gmail.com>
2024-05-16 16:58:27 -0700

    revisions to structure

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    with app.app_context():
+        from app import routes
+
+    return app
